ecda55846c0f6e3b529de211a796caa44fc2ae78
cybermatrixdev <100643071+cybermatrixdev@users.noreply.github.com>
2022-03-24 16:20:06 -0700

    Create follow.js

---
+++ src/pages/Following/follow.js
@@ -0,0 +1,45 @@
+import { signedTypeData, getAddressFromSigner, splitSignature } from './ethers.service';
+import { createFollowTypedData } from './create-follow-typed-data';
+import { lensHub } from './lens-hub';
+
+export const follow = async () => {
+  // hard coded to make the code example clear
+  const followRequest = [
+    {
+       profile: "0x01",
+    },
+    {
+       profile: "0x02",
+       followModule: {
+         feeFollowModule: {
+           amount: {
+             currency: "0xD40282e050723Ae26Aeb0F77022dB14470f4e011",
+             value: "0.01"
+           }
+         }
+       }
+     }
+  ];
+  
+  const result = await createFollowTypedData(followRequest);
+  const typedData = result.data.createFollowTypedData.typedData;
+  
+  const signature = await signedTypeData(typedData.domain, typedData.types, typedData.value);
+  const { v, r, s } = splitSignature(signature);
+  
+  const tx = await lensHub.followWithSig({
+    follower: getAddressFromSigner(),
+    profileIds: typedData.value.profileIds,
+    datas: typedData.value.datas,
+    sig: {
+      v,
+      r,
+      s,
+      deadline: typedData.value.deadline,
+    },
+  });
+  console.log(tx.hash);
+  // 0x64464dc0de5aac614a82dfd946fc0e17105ff6ed177b7d677ddb88ec772c52d3
+  // you can look at how to know when its been indexed here: 
+  //   - https://docs.lens.dev/docs/has-transaction-been-indexed
+}
